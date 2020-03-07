0af685bf86b01dc90a8fb2a6c3713ed164106f40
Christian Leininger <christianleininger42@gmail.com>
2020-03-07 10:44:30 +0100

    update data races

---
+++ 5.Concurrency/DataRaces.cpp
@@ -0,0 +1,45 @@
+// Copyright 2020
+// cpp course
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+#include <iostream>
+#include <thread>
+#include <future>
+
+class Vehicle {
+ public:
+  //default constructor
+  Vehicle() : _id(0), _name( new std::string("Default name")) {
+    std::cout << "Vehicle #" << _id << " Default constructor called" << std::endl;
+  }
+
+  //initializing constructor
+  Vehicle(int id, std::string name) : _id(id), _name(new std::string(name)) {
+    std::cout << "Vehicle #" << _id << " Initializing constructor called" << std::endl;
+  }
+  // setter and getter
+  void setID(int id) { _id = id; }
+  int getID() { return _id; }
+  void setName(std::string name) { *_name = name; }
+  std::string getName() {return *_name;}
+ private:
+  int _id;
+  std::string *_name;
+};
+
+int main() {
+  // create instances of class Vehicle
+  Vehicle v0; // default constructor
+  Vehicle v1(1, "Vehicle 1"); // initializing constructor
+
+  // read and write name in different threads (which one of the above creates a data race?)
+  std::future<void> ftr = std::async([](Vehicle v) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
+      v.setName("Vehicle 2");
+      }, v0);
+
+  v0.setName("Vehicle 3");
+  ftr.wait();
+  std::cout << "Vehicle #" << v0.getName() << std::endl;
+  return 0;
+}
